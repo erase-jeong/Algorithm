0577b9d7fea0261393b4083414f30ddb0d514b25
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-02-25 04:36:45 +0900

    [level 0] Title: 문자열 돌리기, Time: 1.72 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(int i=0;i<size(str);i++){
+        cout<<str[i]<<endl;
+    }
+    return 0;
+}
