8aa02a04748afbb5abf9b6b72d785f290627595a
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-02-29 00:17:48 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,7 +1,6 @@
 def solution(n, control):
+    answer=n
+    c={'w':1, 's':-1, 'd':10, 'a':-10}
     for i in control:
-        if i=="w": n+=1
-        elif i=="s": n-=1
-        elif i=="d": n+=10
-        else: n-=10
-    return n
+        answer+=c[i]
+    return answer
