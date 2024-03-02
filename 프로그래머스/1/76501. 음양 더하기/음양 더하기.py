8085ae1ecc6097e08ae154cfbcb5ea9613216a0b
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-03-02 18:42:12 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer=0
+    for i in range(len(signs)):
+        if signs[i]==True:
+            answer+=absolutes[i]
+        else:
+            answer+=(-1)*absolutes[i]
+            
+    return answer
