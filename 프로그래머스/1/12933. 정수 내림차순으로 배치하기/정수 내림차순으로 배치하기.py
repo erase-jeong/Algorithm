2fdf356bb8dd5d7d9f0ae899e4d66e3a70c1c456
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-03-02 16:51:47 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,9 +1,4 @@
 def solution(n):
-    ap=[]
-    k=''
-    for i in range(len(str(n))):
-        ap.append(str(n)[i])
-    ap.sort()
-    for i in range(1,len(ap)+1):
-        k+=(ap[len(ap)-i])
-    return int(k)
+    ls=list(str(n))
+    ls.sort(reverse=True)
+    return int("".join(ls))
