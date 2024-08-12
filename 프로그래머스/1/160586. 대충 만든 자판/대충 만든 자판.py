d9b8cdaddff0869957f10f2febb89bb04a01869a
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-08-12 23:37:59 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.53 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,17 @@
+def solution(keymap, targets):
+    answer = []
+    hs = {}
+    for k in keymap:
+        for i, ch in enumerate(k):
+            hs[ch] = min(i + 1, hs[ch]) if ch in hs else i + 1
+
+    for i, t in enumerate(targets):
+        ret = 0
+        for ch in t:
+            if ch not in hs:
+                ret = - 1
+                break
+            ret += hs[ch]
+        answer.append(ret)
+
+    return answer
