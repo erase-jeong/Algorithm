a7272f76688cf30bcb9da84dd08982acfa2bc394
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-11-03 03:11:10 +0900

    [D3] Title: 직사각형 길이 찾기, Time: 263 ms, Memory: 49,348 KB -BaekjoonHub

---
+++ SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.py
@@ -0,0 +1,9 @@
+T=int(input())
+for tc in range(1,T+1):
+    a,b,c=map(int,input().split())
+    if(a==b):
+        print(f'#{tc} {c}')
+    elif(a==c):
+        print(f'#{tc} {b}')
+    else:
+        print(f'#{tc} {a}')
