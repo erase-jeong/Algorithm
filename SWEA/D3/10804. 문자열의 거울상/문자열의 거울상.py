eff476da87dc7abc40cdc28fcb85aa5e8d9453c8
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-11-03 15:56:08 +0900

    [D3] Title: 문자열의 거울상, Time: 148 ms, Memory: 48,400 KB -BaekjoonHub

--- SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
+++ SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
@@ -1,17 +1,17 @@
 T=int(input())
-for tc in range(1,T+1):
-    input_str=input()
-    ans=''
-    for i in range(len(input_str)):
-        if(input_str[len(input_str)-i-1]=='p'):
-            ans+='q'
-        elif(input_str[len(input_str)-i-1]=='q'):
-            ans+='p'
-        elif(input_str[len(input_str)-i-1]=='b'):
-            ans+='d'
-        elif(input_str[len(input_str)-i-1]=='d'):
-            ans+='b'
-            
-        #reverse_str+=input_str[len(input_str)-i-1]
+for test_case in range(1,T+1):
+    s=list(input())
 
-    print(f'#{tc} {ans}')
+    for i in range(len(s)):
+        if s[i]=='b':
+            s[i]='d'
+        elif s[i]=='d':
+            s[i]='b'
+        elif s[i]=='p':
+            s[i]='q'
+        else:
+            s[i]='p'
+        
+    s.reverse()
+    s=''.join(s)
+    print("#%d %s"%(test_case,s))
