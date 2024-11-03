86c8c53aa01f4551d5f67c875c0feff1ce9e0037
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-11-03 11:59:14 +0900

    [D3] Title: 홀수일까 짝수일까, Time: 433 ms, Memory: 48,544 KB -BaekjoonHub

--- SWEA/D3/5549. 홀수일까 짝수일까/홀수일까 짝수일까.py
+++ SWEA/D3/5549. 홀수일까 짝수일까/홀수일까 짝수일까.py
@@ -1,7 +1,9 @@
 T=int(input())
 for tc in range(1,T+1):
-    input_integer=int(input())
-    if(input_integer%2==1):
+    input_number=input().strip() #숫자를 문자열로 입력받음
+
+    #마지막 자릿수로 짝수/홀수 판단
+    if input_number[-1] in '13579': #홀수이면
         print(f'#{tc} Odd')
-    else:
+    else: #짝수이면
         print(f'#{tc} Even')
