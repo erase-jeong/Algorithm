aa0b4181f6faecfe77c57e686ecae649cc46d891
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2025-03-20 23:20:11 +0900

    [level 2] Title: 타겟 넘버, Time: 423.89 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,3 @@
-def dfs(numbers, target, index, current_sum):
-    if index == len(numbers):  # 모든 숫자를 사용한 경우
-        return 1 if current_sum == target else 0  # 타겟 넘버 달성 여부 체크
-    
-    # 현재 숫자를 더하는 경우와 빼는 경우를 모두 탐색 (재귀)
-    return (dfs(numbers, target, index + 1, current_sum + numbers[index]) +
-            dfs(numbers, target, index + 1, current_sum - numbers[index]))
-
 def solution(numbers, target):
-    return dfs(numbers, target, 0, 0)
-
+    answer = 0
+    return answer
