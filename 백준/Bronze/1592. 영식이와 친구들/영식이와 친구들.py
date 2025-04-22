60229ad0012c8e4d4a9d8b183e87b7f79f81cec5
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-04-23 08:34:55 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
@@ -2,9 +2,9 @@ N,M,L=map(int,input().split())
 balls=[0]*(N+1)
 count,now=0,0
 
-#while(max(balls)!=M):
-while(max(balls)<M):
-    #print("공 위치 : ",{now+1})
+while(max(balls)!=M):
+#while(max(balls)<M):
+    #print("공 위치 : ",{now+1})  -> 이거 있으면 출력초과
     
     if balls[now]%2==0:  # 실제론 홀수 (0번 인덱스부터여서)
         balls[now]+=1
