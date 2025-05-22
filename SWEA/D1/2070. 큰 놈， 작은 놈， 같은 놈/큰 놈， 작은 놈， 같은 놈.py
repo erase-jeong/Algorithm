e5eea489e0ebc8341d04774bc17f468375d67a17
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-05-22 21:00:20 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 64 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
@@ -1,8 +1,5 @@
 T=int(input())
 for i in range(1,T+1):
     a,b=map(int,input().split())
-    print(f"#{i}",end=" ")
-    if a>b:print(">")
-    elif a<b:print("<")
-    else:
-        print("=")
+    result = ">" if a > b else "<" if a < b else "="
+    print(f"#{i} {result}")
