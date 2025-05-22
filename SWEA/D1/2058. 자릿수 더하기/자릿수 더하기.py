93a7651e30fd2cf92204f3569efdbc9e18d45b59
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-05-22 21:14:42 +0900

    [D1] Title: 자릿수 더하기, Time: 56 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,10 +1,5 @@
-N=int(input())
-#최대 네자리
-#각 자리수 나누면서...
-
-il=N%10
-sib=((N%1000)%100)//10
-bak=(N%1000)//100
-chan=N//1000
-
-print(il+sib+bak+chan)
+N=input()
+total=0
+for digit in N:
+    total+=int(digit)
+print(total)
