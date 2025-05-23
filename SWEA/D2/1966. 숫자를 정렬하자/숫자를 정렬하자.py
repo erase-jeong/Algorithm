de495557abbdd8a5cc9476a981ebe58f59dd6aea
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-05-23 23:07:18 +0900

    [D2] Title: 숫자를 정렬하자, Time: 65 ms, Memory: 51,840 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -3,7 +3,4 @@ for tc in range(1,T+1):
     N=int(input())
     data=list(map(int,input().split()))
     data.sort()
-    data_str=""
-    for i in data:
-        data_str+=str(i)+" "
-    print(f'#{tc} {data_str}')
+    print(f'#{tc}',*data)
