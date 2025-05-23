a71cbae3bdab1dfb697a46418f89ebcfb86068e5
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-05-23 16:23:46 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 78 ms, Memory: 59,776 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -0,0 +1,10 @@
+T=10
+for test_case in range(1,T+1):
+    N=int(input())
+    lst=list(map(int,input().split()))
+    ans=0
+    for i in range(2,N-2):
+        mx=max(lst[i-2:i]+lst[i+1:i+3]) #4개 값을 직접 써줘도 됨
+        if lst[i]>mx:
+            ans+=lst[i]-mx
+    print(f'#{test_case}: {ans}')
