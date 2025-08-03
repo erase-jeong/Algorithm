73290f4b175d09e04b3c73d261a0c9884fe4378f
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-08-03 11:19:35 +0900

    [Silver I] Title: 절댓값 힙, Time: 428 ms, Memory: 27024 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        //N 입력
+        int N=Integer.parseInt(br.readLine());
+
+        //우선순위 큐 선언
+        PriorityQueue<Integer> MyQueue=new PriorityQueue<>((o1,o2)->{
+           int first_abs=Math.abs(o1);
+           int second_abs=Math.abs(o2);
+           if(first_abs==second_abs)
+               return o1>o2?1:-1; //절댓값이 같으면 음수 우선 정렬하기
+           else
+               return first_abs-second_abs; //절댓값을 기준으로 정렬하기
+        });
+
+        for(int i=0;i<N;i++){
+            int request=Integer.parseInt(br.readLine());
+
+            if(request==0){  //요청이 0일 때
+                if(MyQueue.isEmpty()){
+                    System.out.println("0");
+                }
+                else{
+                    System.out.println(MyQueue.poll());
+                }
+            }
+            else{  //요청이 1일 떄
+                MyQueue.add(request);
+            }
+        }
+    }
+}
