fe34647ac2065a693efc50421f8b572fe0e77f89
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-09-28 08:35:41 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,30 @@
-import java.util.Scanner;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int K = in.nextInt();
-		
-		int[] coin = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			coin[i] = in.nextInt();
-		}
-		
-		int count = 0;
- 
-		for(int i = N - 1; i >= 0; i--) {
- 
-			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-			if(coin[i] <= K) {
-				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-				count += (K / coin[i]);
-				K = K % coin[i];
-			}
-		}
-		System.out.println(count);
-	}
- 
+    public static void main(String[] args) throws IOException{
+        int answer=0;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+
+        int[] coins=new int[n];
+
+        for(int i=0;i<n;i++){
+            coins[i]=Integer.parseInt(br.readLine());
+        }
+
+        //반대기준으로
+        for(int i=n-1;i>=0;i--){
+            answer+=k/coins[i];
+            k%=coins[i];
+            //System.out.println(coins[i]+" "+answer+" "+k);
+        }
+        
+        System.out.println(answer);
+    }
 }
+
+
