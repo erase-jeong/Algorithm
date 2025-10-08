d06f59665dc19ed183ac478c9f2be153d3e33563
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-10-08 13:22:49 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        
+        int n=0;
+        
+        for(boolean b : finished){
+            if(b==false) n++;
+        }
+        
+        String[] answer = new String[n];
+        
+        int j=0;
+        for(int i=0;i<todo_list.length;i++){
+            if(finished[i]==false){
+                answer[j]=todo_list[i];
+                j++;
+            }
+        }
+        return answer;
+    }
+}
+
+/*
+1. finished 에서 false의 개수를 센다 => 정답 배열 만들기
+2. 반복문 돌면서 넣기
+*/
