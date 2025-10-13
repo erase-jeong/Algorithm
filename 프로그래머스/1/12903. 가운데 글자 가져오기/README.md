10df5e09db59cfbe8103838a95cf7ac18cdae32c
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-10-14 00:12:45 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
 
 ### 성능 요약
 
-메모리: 80.9 MB, 시간: 0.04 ms
+메모리: 85.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 30일 19:29:37
+2025년 10월 14일 00:12:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,16 +1,26 @@
-class Solution {
-    public String solution(String s) {
-        
+class Solution{
+    public String solution(String s){
+        String answer="";
         int mid=s.length()/2;
         
-        if(s.length()%2==0){
-            //짝수 길이 -> 가운데 2글가
-            return s.substring(mid-1,mid+1);
+        if(s.length()%2==1){
+            answer=s.substring(mid,mid+1);
         }
-        else{
-            //홀수 길이 -> 가운데 1글자
-            return String.valueOf(s.charAt(mid));
+        else{  //짝수
+            answer=s.substring(mid-1,mid+1);
         }
         
+        return answer;   
     }
 }
+
+/*
+홀수
+0 1 2 3 4 
+mid=길이/2 =>2 , 2번인덱스 ok
+
+짝수
+0 1 2 3 4 5
+mid=3번 + -1번인덱스
+mid-1 ~ mid 
+*/
