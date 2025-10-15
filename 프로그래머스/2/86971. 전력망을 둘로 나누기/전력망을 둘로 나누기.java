cb2831ba68b39296e3fe3f1afab5a544976dd34a
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-10-15 10:56:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.72 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,60 @@
 import java.util.*;
 
-class Solution {
-    static HashMap<Integer, List<Integer>> map;
-    static boolean[] visited;
-
-    public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-
-        // 1. 그래프(양방향) 구성
-        map = new HashMap<>();
-        for (int[] w : wires) {
-            int a = w[0];
-            int b = w[1];
-
-            map.computeIfAbsent(a, k -> new ArrayList<>()).add(b);
-            map.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
+class Solution{
+    
+    static ArrayList<Integer>[] graph;
+    static int min;
+    
+    public int solution(int n, int[][] wires){
+        graph=new ArrayList[n+1];
+        min=Integer.MAX_VALUE;
+          
+        //그래프 ArrayList 초기화. 노드 개수 만큼 ArrayList 생성
+        for(int i=1;i<=n;i++){
+            graph[i]=new ArrayList<>();
         }
-
-        // 2. 하나씩 끊어보기
-        for (int[] w : wires) {
-            int a = w[0];
-            int b = w[1];
-
-            // 끊은 간선을 제외하기 위해 visited 초기화
-            visited = new boolean[n + 1];
-
-            // 한 쪽 트리의 크기를 구함
-            int area1 = dfs(a, b);
-            int area2 = n - area1;
-
-            int diff = Math.abs(area1 - area2);
-            answer = Math.min(answer, diff);
+        
+        
+        //양방향 간선 구조이므로 두 번 add를 해준다.
+        for(int i=0;i<wires.length;i++){
+            int v1=wires[i][0];
+            int v2=wires[i][1];
+            graph[v1].add(v2);
+            graph[v2].add(v1);
         }
-
-        return answer;
+        
+        for(int i=0;i<wires.length;i++){
+            int v1=wires[i][0];
+            int v2=wires[i][1];
+            
+            boolean[] visited=new boolean[n+1];
+            
+            //해당 간선을 그래프에서 제거
+            graph[v1].remove(Integer.valueOf(v2));
+            graph[v2].remove(Integer.valueOf(v1));
+            
+            int cnt=dfs(1,visited); //임의의 시작점에서 dfs 탐색
+            int diff=Math.abs(cnt-(n-cnt));
+            min=Math.min(min,diff);
+            
+            //그래프에 다시 간선 추가
+            graph[v1].add(v2);
+            graph[v2].add(v1);
+        }
+        
+        return min;
     }
-
-    // DFS: 특정 노드에서 탐색 가능한 송전탑 개수 반환
-    private int dfs(int node, int cut) {
-        visited[node] = true;
-        int count = 1; // 자기 자신 포함
-
-        for (int next : map.get(node)) {
-            // 끊은 간선 방향은 건너뛰기
-            if (next == cut || visited[next]) continue;
-            count += dfs(next, node);
+    
+    static int dfs(int v, boolean[] visited){
+        visited[v]=true;
+        int cnt=1;
+        
+        for(int next:graph[v]){
+            if(!visited[next]){
+                cnt+=dfs(next, visited);
+            }
         }
-
-        return count;
+        
+        return cnt;
     }
 }
