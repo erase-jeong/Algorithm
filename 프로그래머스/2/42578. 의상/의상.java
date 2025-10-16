0ee0eeb49e0aee29cd0a50f00de33635372b2206
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-10-16 20:19:38 +0900

    [level 2] Title: 의상, Time: 0.62 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,39 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer=1;
-        HashMap<String, List<String>> map=new HashMap<>();
         
+        //HashMap 선언
+        HashMap<String,List<String>> map=new HashMap<>();
+        
+        //값 넣기
         for(int i=0;i<clothes.length;i++){
-            if(!map.containsKey(clothes[i][1])){
-                List<String> lst=new ArrayList<>();
-                lst.add(clothes[i][0]);
-                map.put(clothes[i][1],lst);
-            }else{
-                List<String> lst=new ArrayList<>();
-                lst=map.get(clothes[i][1]);
-                lst.add(clothes[i][0]);
-                map.put(clothes[i][1],lst);
-            }
+            String name=clothes[i][0];
+            String type=clothes[i][1];
+            map.computeIfAbsent(type,k->new ArrayList<>()).add(name);
         }
+    
+        
+        //결과 만들기
         
         for(String s : map.keySet()){
-            List<String> lst2=new ArrayList<>();
-            lst2=map.get(s);
-            int size=lst2.size();
-            answer*=size+1;            
+            answer*=map.get(s).size()+1;
         }
         
-        answer-=1;
-        return answer;
+        
+        return answer-1;
     }
 }
-
-/*
-*해시맵 이용
-<key, value> set = {};
-key에 두번째 값 넣고 없으면 추가하고
-headgear : {yellow_hat, green_turban},
-eyewear : {blue_sunglasses}
-
-해시맵? 해시셋? 셋은 그냥 셋일텐데?
-*/
