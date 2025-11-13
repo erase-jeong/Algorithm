245fd688c1081e5d5b95c872d60ff4432b556592
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-11-13 17:53:54 +0900

    [Gold IV] Title: 문자열 폭발, Time: 480 ms, Memory: 33944 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        String target=br.readLine();
+
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            st.push(str.charAt(i)); //문자를 stack에 넣는다.
+
+            if(st.size()>=target.length()){
+                boolean flag=true;
+                for(int j=0;j<target.length();j++){ //stack에 들어있는 문자들을 target과 같은지 비교한다.
+                    if(target.charAt(j)!=st.get(st.size()-target.length()+j)){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){  //3. 만약 같으면 stack 에서 pop()한다.
+                    for(int k=0;k<target.length();k++){
+                        st.pop();
+                    }
+                }
+
+            }
+        }
+
+        StringBuilder sb=new StringBuilder();
+        if(st.isEmpty()){
+            sb.append("FRULA");
+        }else{
+            for(char c : st){
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb.toString());
+        br.close();
+
+    }
+}
