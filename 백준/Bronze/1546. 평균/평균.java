d888635518f6876d199e1505ed79a82e7a6707f9
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-11-13 13:19:32 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18748 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,18 +1,31 @@
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
-        
-        long sum=0;
-        long max=0;
-        
-        for(int i=0;i<N;i++){
-            int temp=sc.nextInt();
-            if(temp>max) max=temp;
-            sum=sum+temp;
+
+        int n=sc.nextInt();
+        sc.nextLine();
+        int[] arr=new int[n];
+
+        int maxV=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]>maxV){
+                maxV=arr[i];
+            }
         }
-        System.out.println(sum*100.0/max/N);
+
+        //System.out.println("Max : "+maxV);
+        //System.out.println(Arrays.toString(arr));
+
+        float sumV=0;
+
+        for(int i=0;i<n;i++){
+            sumV+=(float)arr[i]/(float)maxV*(float)100;
+        }
+
+        //System.out.println("sumV : "+sumV);
+        System.out.println(sumV/n);
     }
 }
