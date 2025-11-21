148a0e27712faa9c00373647620cc8cbeb01da44
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-11-21 16:56:50 +0900

    [Bronze II] Title: 상수, Time: 140 ms, Memory: 16244 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,27 +1,30 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        String b=sc.next();
+    public static void main(String args[]) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
+        String a=st.nextToken();
+        String b=st.nextToken();
         String aa="";
         String bb="";
 
         for(int i=a.length()-1;i>=0;i--){
             aa+=a.charAt(i);
+            //System.out.println(aa);
         }
 
         for(int i=b.length()-1;i>=0;i--){
             bb+=b.charAt(i);
+            //System.out.println(bb);
         }
 
         if(Integer.parseInt(aa)>Integer.parseInt(bb)){
-            System.out.println(Integer.parseInt(aa));
-        }
-        else{
-            System.out.println(Integer.parseInt(bb));
+            System.out.println(aa);
+        }else{
+            System.out.println(bb);
         }
     }
 }
