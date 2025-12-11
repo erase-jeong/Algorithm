248782d90f63f8fdf510da81aa864aaac0713a8a
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-12 03:05:08 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 100 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static int N;
+    public static int[] arr;
     public static int maxVal;
     public static int minVal;
 
-    public static int[] arr;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         N=Integer.parseInt(br.readLine());
         arr=new int[N];
-
         StringTokenizer st=new StringTokenizer(br.readLine());
+
         for(int i=0;i<N;i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
 
-        int[] oper=new int[4]; // + - * %
+        int[] oper=new int[4];
+
         st=new StringTokenizer(br.readLine());
+
         for(int i=0;i<4;i++){
             oper[i]=Integer.parseInt(st.nextToken());
         }
@@ -33,47 +30,47 @@ public class Main {
         maxVal=Integer.MIN_VALUE;
         minVal=Integer.MAX_VALUE;
 
-        dfs(1,arr[0],oper[0], oper[1], oper[2], oper[3]);
-        
+        dfs(1, arr[0], oper[0], oper[1],oper[2],oper[3]);
 
         System.out.println(maxVal);
         System.out.println(minVal);
-
     }
 
-    public static void dfs(int idx, int cur, int plus, int minus, int mul, int div){
-        //idx : 지금 몇번째 숫자를 사용할 차례인지 (0-based)
-        //cur : 지금까지 계산된 값
+    public static void dfs(int idx, int result, int plus, int minus, int mul, int div){
 
-        if(idx==N){ //숫자를 전부 다 썼으면
-            maxVal=Math.max(maxVal, cur);
-            minVal=Math.min(minVal, cur);
+        //종료조건
+        if(idx==N){
+            maxVal=Math.max(maxVal,result);
+            minVal=Math.min(minVal,result);
             return;
+
         }
 
         int nextNum=arr[idx];
 
-        //1)더하기
-        if(plus>0) dfs(idx+1,cur+nextNum, plus-1, minus, mul, div);
+        if(plus>0){
+            dfs(idx+1, result+nextNum, plus-1, minus, mul, div);
+        }
 
-        //2)빼기
-        if(minus>0) dfs(idx+1, cur-nextNum,plus, minus-1, mul, div);
+        if(minus>0){
+            dfs(idx+1, result-nextNum, plus, minus-1, mul, div);
+        }
 
-        //3)곱하기
-        if(mul>0) dfs(idx+1, cur*nextNum, plus, minus, mul-1, div);
+        if(mul>0){
+            dfs(idx+1,result*nextNum, plus, minus, mul-1, div);
+        }
 
-        //4)나누기
         if(div>0){
+
             int temp;
-            if(cur<0){
-                temp=-(Math.abs(cur)/nextNum);
+            if(nextNum>0){
+                temp=result/nextNum;
             }else{
-                temp=cur/nextNum;
+                temp=-(Math.abs(result)/nextNum);
             }
             dfs(idx+1, temp, plus, minus, mul, div-1);
+
         }
 
     }
-
-
 }
