fc113ea6a26c8b01831d095d190627dc81f19707
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-12 17:35:37 +0900

    [level 2] Title: 모음 사전, Time: 22.14 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
-class Solution {
+class Solution{
     static List<String> list;
     static String[] words={"A","E","I","O","U"};
     
-    public int solution(String word) {
-        int answer = 0;
+    public static int solution(String find){
+        int answer=0;
         list=new ArrayList<>();
         dfs("",0);
-        System.out.println("list : "+list);
         int size=list.size();
-        System.out.println("size : "+size);
         
         for(int i=0;i<size;i++){
-            if(list.get(i).equals(word)){
+            if(list.get(i).equals(find)){
                 answer=i;
                 break;
             }
         }
+        
         return answer;
     }
     
-    static void dfs(String str, int len){
+    public static void dfs(String str, int len){
         list.add(str);
         if(len==5) return;
+        
         for(int i=0;i<5;i++){
             dfs(str+words[i],len+1);
         }
