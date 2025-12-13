ec6144b7918ba6da42787e114a5f009e2d29f514
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-14 04:48:35 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,34 +1,22 @@
 import java.util.*;
 
-class Solution {
-    public int solution(String[][] clothes) {
-        HashMap<String, Integer> map=new HashMap<>();
-        
-        //종류별 개수 세기
+class Solution{
+    public int solution(String[][] clothes){
+        HashMap<String,Integer> map=new HashMap<>();
         for(int i=0;i<clothes.length;i++){
             String category=clothes[i][1];
             
-            if(map.containsKey(category)){
-                map.put(category,map.get(category)+1);
-            }else{
+            if(!map.containsKey(category)){
                 map.put(category,1);
+            }else{
+                map.put(category,map.get(category)+1);
             }
         }
         
-        //조합 계산
         int answer=1;
-        for(int count:map.values()){
-            answer*=(count+1); //입지 않는 경우 포함
-        }
-        
-        //아무것도 안입은 경우 제외
+        for(String key :map.keySet()){
+            answer*=(map.get(key)+1);
+        }    
         return answer-1;
     }
 }
-
-
-/*
-if clothes[i][1]에 해당하는 값이 hashmap에 있는지 확인하기
-    => 없으면? key 추가하기
-    => 있으면? value넣기
-*/
