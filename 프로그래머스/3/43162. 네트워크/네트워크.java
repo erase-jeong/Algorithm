9760a310314922b8fd71492abbf6dff722d7a633
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-14 15:47:31 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,11 +1,12 @@
 class Solution {
+    static boolean[] check;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] check = new boolean[n];
+        check = new boolean[n];
         
         for(int i=0;i<n;i++){
             if(!check[i]){
-                dfs(computers, i, check);
+                dfs(computers, i);
                 answer++;
             }
         }
@@ -13,12 +14,12 @@ class Solution {
         return answer;
     }
     
-    public void dfs(int[][] computers, int i, boolean[] check){
+    public void dfs(int[][] computers, int i){
         check[i] = true;
         
         for(int j=0;j<computers.length;j++){
             if(i != j && computers[i][j] == 1 && !check[j]){
-                dfs(computers, j, check);
+                dfs(computers, j);
             }
         }
     }
