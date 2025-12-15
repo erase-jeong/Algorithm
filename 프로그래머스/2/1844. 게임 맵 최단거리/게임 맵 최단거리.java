ff0f077f8e331f5f19e12f97deecef1c88d669ed
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-15 21:50:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.16 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,44 @@
 import java.util.*;
 
-class Solution {
-    private static final int[] dx = {0, 0, -1, 1};
-    private static final int[] dy = {-1, 1, 0, 0};
+class Solution{
     
-    public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
+    public int solution(int[][] maps){
+        int n=maps.length;
+        int m=maps[0].length;
         
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         
-        while(!queue.isEmpty()){ // bfs
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            int cnt = current[2];
+        Queue<int []> queue=new LinkedList<>();
+        boolean[][] visited=new boolean[n][m];
+        
+        
+        queue.add(new int[]{0,0,1});
+        
+        while(!queue.isEmpty()){
+            int[] p=queue.poll();
+            int x=p[0];
+            int y=p[1];
+            int cnt=p[2];
+            //visited[x][y]=true;  //(1)
             
-            if(x == n-1 && y == m-1){
-                return cnt; // 최단 거리 반환 (bfs: 가장 처음 return 되는게 최단 거리)
+            if(x==n-1 && y==m-1){
+                return cnt;
             }
             
             for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx=x+dx[i];
+                int ny=y+dy[i];
                 
-                if(nx >=0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    queue.add(new int[]{nx, ny, cnt+1});
+                if(nx>=0 && ny>=0 && nx<n && ny<m && maps[nx][ny]==1 && !visited[nx][ny]){
+                    visited[nx][ny]=true;  //같은 값 다시 큐에 넣지 않기 위해서 
+                    queue.add(new int[]{nx,ny,cnt+1});
                 }
+                
             }
+            
         }
-        
         return -1;
     }
+    
 }
