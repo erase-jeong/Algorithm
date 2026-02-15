e75deba8f1a9e4869a876df40a1288593c5c2972
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-02-15 21:40:28 +0900

    [Bronze V] Title: 채플이 너무해, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        if(n>=6) System.out.println("Success!");
+        else System.out.println("Oh My God!");
+    }
+}
