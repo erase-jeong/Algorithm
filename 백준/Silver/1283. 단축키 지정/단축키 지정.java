c2fba22199464460dab12cefb0b7a1ff1718aa20
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-02-25 14:09:00 +0900

    [Silver I] Title: 단축키 지정, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -1,85 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine());
 
         List<String> options=new ArrayList<>();
-
         for(int i=0;i<N;i++){
             options.add(br.readLine());
         }
 
-        //System.out.println(options);
+        //존재 여부만 확인하므로 HashMap 대신 HashSet 사용
+        HashSet<Character> usedKeys=new HashSet<>();
 
-        HashMap<Character,Integer> map=new HashMap<>();
-
-        //5개이하의 단어 , 1단어 = 10개이하 알파벳 => 공백포함 최대 55번? 54번?
+        //출력을 한 번에 모아서 하기 위한 StringBuilder
+        StringBuilder sb=new StringBuilder();
 
         for(int i=0;i<N;i++){
             String option=options.get(i);
-            //String[] words=o.split(" ");
-            //이거말고 각 단어의 첫번째 인덱스를 배열에 저장해두는게 더 편할꺼같은데?
             ArrayList<Integer> wordStartIndex=new ArrayList<>();
-            //System.out.println("option : "+option);
 
+            //단어의 시작 인덱스 찾기
             for(int j=0;j<option.length();j++){
-                if(option.charAt(j)==' '){ //공백은 단어의 시작이 될 수 없으므로 패스
+                if(option.charAt(j)==' '){
                     continue;
                 }
-                //첫 번째 글자이거나, 바로 앞 글자가 공백인 경우 단어의 시작점
                 if(j==0 || option.charAt(j-1)==' '){
                     wordStartIndex.add(j);
                 }
             }
-            //System.out.println("wordStartIndex : "+wordStartIndex);
 
-            int targetIdx=-1; //단축키로 지정된 인덱스를 저장할 변수. -1이면 아직 못 찾았다는 뜻
-            int idx=wordStartIndex.get(0);
-            for(int kk=0;kk<wordStartIndex.size();kk++){
-                int realIdx=wordStartIndex.get(kk);
-                char c=option.charAt(realIdx);
-                char upperC=Character.toUpperCase(c); //대소문자 구분을 없애기 위해 대문자로 변환
+            int targetIdx=-1;
 
-                //맵에 대문자로 변환한 글자가 없다면 단축키로 지정 가능
-                if(!map.containsKey(upperC)){  //없다면 넣는거니깐 이미 있다면 if문 작동안하고 for반복문으로 돈다.
-                    map.put(upperC,realIdx);
+            //1단계 : 각 단어의 첫 글자 확인
+            for(int j=0;j<wordStartIndex.size();j++){
+                int realIdx=wordStartIndex.get(j);
+                char upperC=Character.toUpperCase(option.charAt(realIdx));
+
+                if(!usedKeys.contains(upperC)){
+                    usedKeys.add(upperC);
                     targetIdx=realIdx;
                     break;
                 }
             }
 
+            //2단계 : 첫 글자에서 못 찾았다면, 왼쪽부터 차례대로 남은 알파벳 검사
             if(targetIdx==-1){
-                for(int k=0;k<option.length();k++){  
-                    //1. 만약 option.charAt(k)가 공백(' ')이라면 건너뛰기 (continue)
-                    if(option.charAt(k)==' ') continue;
-                    //2. 공백이 아니라면 대문자로 변환하기
-                    char c=option.charAt(k);
-                    char upperC=Character.toUpperCase(c);
+                for(int j=0;j<option.length();j++){
+                    if(option.charAt(j)==' ') continue;
 
-                    //3. 맵에 그 대문자가 없다면 단축키로 지정 가능!
-                    if(!map.containsKey(upperC)){
-                        map.put(upperC,k);
-                        targetIdx=k;
+                    char upperC=Character.toUpperCase(option.charAt(j));
+
+                    if(!usedKeys.contains(upperC)){
+                        usedKeys.add(upperC);
+                        targetIdx=j;
                         break;
                     }
-                       
                 }
+
             }
- 
-            for(int k=0;k<option.length();k++){
-                if(k==targetIdx){
-                    System.out.print("["+option.charAt(k)+"]");
+
+            //3단계 : 출력 문자열 만들기 (StringBuilder에 쌓기)
+            for(int j=0;j<option.length();j++){
+                if(j==targetIdx){
+                    sb.append("[").append(option.charAt(j)).append("]");
                 }else{
-                    System.out.print(option.charAt(k));
+                    sb.append(option.charAt(j));
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
 
+        //모아둔 결과물 한 번에 출력
+        System.out.println(sb.toString());
     }
 }
