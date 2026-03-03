6ed878f7816159a0cabf442ab797e984473fae63
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-03-04 08:29:01 +0900

    [Silver III] Title: 주유소, Time: 408 ms, Memory: 36488 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,12 +4,12 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     static int N;
-    static int[] road;
-    static int[] oil;
+    static long[] road;
+    static long[] oil;
 
-    static int money;
+    static long money;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +18,7 @@ public class Main {
         //System.out.println(N);
 
         //도로의 길이  (총 N-1개)
-        road=new int[N-1];
+        road=new long[N-1];
         StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<N-1;i++){
             road[i]=Integer.parseInt(st.nextToken());
@@ -26,58 +26,28 @@ public class Main {
         //System.out.println(Arrays.toString(road));
 
         //주유소 리터당 가격 (N개)
-        oil=new int[N];
+        oil=new long[N];
         st=new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             oil[i]=Integer.parseInt(st.nextToken());
         }
         //System.out.println(Arrays.toString(oil));
 
-        while(true){
-            //System.out.println("---반복문---");
+        ///처음부터 마지막 도시로 이동하기
+        long pickOil=oil[0];
+        /// 첫 번째 도시 처리
+        money+=pickOil*road[0];
 
-            ///가장 저렴한 주유소 찾기
-            int min=Integer.MAX_VALUE;
-            int idx=0; //초기값 뭐로 설정해야하지?
-            for(int i=0;i<N-1;i++){
-                if(oil[i]<min) {
-                    min=oil[i];
-                    idx=i;
-                }
+        /// 두 번째부터 도시 처리
+        for(int i=1;i<N-1;i++){  //종료 부등호 머리가 안돌아간다아
+            //지금 도시가 아까넣었던 곳보다 더 싸다면?
+            if(oil[i]<pickOil){
+                //새로 갱신해서 넣는다..
+                pickOil=oil[i];
             }
-            //System.out.println("주유소 찾기(oil) : "+Arrays.toString(oil));
-            //System.out.println("idx : "+idx+", min : "+min);
-
-            ///이 뒤에부터 거리는 이 가격으로
-            int dist=0; //총 거리  (금액은 위에서 구해둔 min값으로)
-            for(int i=idx;i<N-1;i++){
-                dist+=road[i];
-                road[i]=0;
-                oil[i]=Integer.MAX_VALUE;
-            }
-            money+=min*dist;
-            //System.out.println("money : "+money);
-            //System.out.println("road : "+Arrays.toString(road));
-            //System.out.println("oil : "+Arrays.toString(oil));
-
-            if(isFinish()){
-                System.out.println(money);
-                break;
-            }
-            //System.out.println("종료 실패!");
-
+            money+=pickOil*road[i];
         }
+        System.out.println(money);
     }
 
-    //내가 이런 로직처리를 너무 비효율적으로 함. 단점
-    static boolean isFinish(){
-        int cnt=0;
-        for(int i=0;i<N-1;i++){
-            if(road[i]==0){
-                cnt++;
-            }
-        }
-        if(cnt==N-1) return true;
-        else return false;
-    }
 }
