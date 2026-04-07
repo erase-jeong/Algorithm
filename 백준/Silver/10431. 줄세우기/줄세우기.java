8fc855268998eda5d790b7afe4c21e43eb264e51
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-04-07 14:15:36 +0900

    [Silver V] Title: 줄세우기, Time: 268 ms, Memory: 19912 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -2,33 +2,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        for(int i=0;i<N;i++){
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+
+        for(int tc=1;tc<=N;tc++){
             StringTokenizer st=new StringTokenizer(br.readLine());
-            List<Integer> lst=new ArrayList<>();
-            
+            int num=Integer.parseInt(st.nextToken());
+
+            //int[] arr=new int[20];
+            ArrayList<Integer> lst=new ArrayList<>();
+
             int cnt=0;
-            int number=0;
-            for(int j=0;j<21;j++){
-                if(j==0) number=Integer.parseInt(st.nextToken());
-                else{
-                    int value=Integer.parseInt(st.nextToken());
-                    int end=lst.size();
-                    lst.add(value);
-                    Collections.sort(lst);
-                    int index=lst.indexOf(value);
-                    cnt+=(end-index);
+
+            for(int i=0;i<20;i++){
+                int student=Integer.parseInt(st.nextToken());
+                boolean inserted=false;
+
+                //현재 줄(lst)을 처음부터 확인하며 나보다 큰 학생 찾기
+                for(int j=0;j<lst.size();j++){
+                    if(lst.get(j)>student){
+                        //나보다 큰 학생을 찾음! 그 자리에 끼어들기
+                        cnt+=(lst.size()-j); //내 뒤에 있는 사람 수만큼 걸음 수 추가
+                        lst.add(j,student);
+                        inserted=true;
+                        break; //자리를 찾았으니 다음 학생으로
+                    }
                 }
+
+                //만약 나보다 큰 학생이 한 명도 없었다면? 맨 뒤에 서기
+                if(!inserted){
+                    lst.add(student);
+                }
+
+
             }
-            System.out.println(number+" "+cnt);
+            System.out.println(num+" "+cnt);
+
         }
     }
 }
