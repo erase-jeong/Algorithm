d220e5c0fbb22867ac804b2df9780c2e20473926
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-04-07 14:14:55 +0900

    [Silver V] Title: 줄세우기, Time: 244 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,23 +1,25 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
         for(int i=0;i<N;i++){
             StringTokenizer st=new StringTokenizer(br.readLine());
             List<Integer> lst=new ArrayList<>();
-
+            
             int cnt=0;
             int number=0;
             for(int j=0;j<21;j++){
-                if(j==0){
-                    number=Integer.parseInt(st.nextToken());
-                }
-                else {
+                if(j==0) number=Integer.parseInt(st.nextToken());
+                else{
                     int value=Integer.parseInt(st.nextToken());
                     int end=lst.size();
                     lst.add(value);
@@ -28,6 +30,5 @@ public class Main {
             }
             System.out.println(number+" "+cnt);
         }
-
     }
 }
