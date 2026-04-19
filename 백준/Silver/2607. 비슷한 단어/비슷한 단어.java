de24a19bf52bb574f023607ebb1eb0e79c54d82f
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-04-19 19:04:56 +0900

    [Silver II] Title: 비슷한 단어, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,57 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String firstWord = br.readLine();
+        int answer=0;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        String word=br.readLine();
 
-        int[] firstCount = getFrequency(firstWord);
-        int result = 0;
+        int firstLength=word.length();
+        int[] firstWord=new int[26];
 
-        for (int i = 0; i < N - 1; i++) {
-            String targetWord=br.readLine();
-            int[] targetCount=getFrequency(targetWord);
-
-            int sameCount=0; //공통된 알파벳의 총 개수
-            for(int j=0;j<26;j++){
-                //두 단어 중 더 적게 가진 쪽의 개수를 더함(교집합 개념)
-                sameCount+=Math.min(firstCount[j],targetCount[j]);
-            }
-
-            //비료 로직
-            //1. 두 단어의 길이가 같을 때
-            if(firstWord.length()==targetWord.length()){
-                if(firstWord.length()==sameCount || firstWord.length()-1==sameCount){
-                    result++;
-                }
-            }
-            //2. 기준 단어가 한 글자 더 길 때 (제거하는 경우)
-            else if(firstWord.length()==targetWord.length()+1){
-                if(targetWord.length()==sameCount){
-                    result++;
-                }
-            }
-            //3. 기준 단어가 한 글자 더 짧을 때 (추가하는 경우)
-            else if(firstWord.length()==targetWord.length()-1){
-                if(firstWord.length()==sameCount){
-                    result++;
-                }
-            }
-        }
-        System.out.println(result);
-    }
-
-    //알파벳 빈도수를 구해주는 메소드
-    private static int[] getFrequency(String word){
-        int[] count=new int[26];
         for(int i=0;i<word.length();i++){
-            count[word.charAt(i)-'A']++;
+            firstWord[word.charAt(i)-'A']++;
         }
-        return count;
+
+        for(int i=1;i<N;i++){
+            word=br.readLine();
+            int matchCount=0;
+
+            int[] compareWord=firstWord.clone();
+
+            for(int j=0;j<word.length();j++){
+                int charIdx=word.charAt(j)-'A';
+                if(compareWord[charIdx]>0){
+                    matchCount++;
+                    compareWord[charIdx]--;
+                }
+            }
+
+
+            if(word.length()==firstLength){
+                //1) 완전히 같다 -> matchCount가 원본 단어의 길이와 같다
+                if(matchCount==firstLength) answer++;
+                else if(matchCount==firstLength-1) answer++;
+                //2) 한 문자만 다르다 -> matchCount가 원본 단어의 길이보다 1작다
+                // (ex. GOD vs GOOD 은 길이는 다르지만, ABC vs ABD는 길이는 같고 2개만 일치)
+
+            }else if(word.length()+1==firstLength){
+                if(matchCount==word.length()) answer++;
+                //1) 구성은 같되, 길이가 다르다 (한문자 중복)
+                //2) 구성이 다른 글자 하나가 있다
+            }else if(word.length()-1==firstLength){
+                if(matchCount==firstLength) answer++;
+                //1) 구성은 같고, 길이만 다르다
+                //2) 구성이 다른 애 하나가 있다
+            }
+        }
+        System.out.println(answer);
+
     }
 }
