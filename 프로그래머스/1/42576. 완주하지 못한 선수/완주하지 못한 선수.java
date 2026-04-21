2c136b0fbdcfddb4a5cf0037efbec02da7e3bc66
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-04-21 20:07:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.26 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,27 +3,22 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer="";
-        
-        //1. Hash map을 만든다. (participant)
         HashMap<String,Integer> map=new HashMap<>();
         
-        for(String player:participant)
-            map.put(player,map.getOrDefault(player,0)+1);
-        
-        //2. Hash map을 뺀다.
-        for(String player:completion)
-            map.put(player,map.get(player)-1);
-        
-        //3. value가 0이 아닌 마지막 주자를 찾는다.
-        //System.out.println(map.keySet());
+        for(String p:participant){
+            if(!map.containsKey(p)) map.put(p,1); //key가 없으면 0을 넣어라
+            else map.put(p,map.get(p)+1); //key가 있으면 +1한 값을 넣어라            
+        }
+        for(String c:completion){
+            map.put(c,map.get(c)-1); //완주한 사람은 -1
+        }
         
         for(String key:map.keySet()){
-            if(map.get(key)!=0){
-                answer=key;
-                break;
-            }
+            if(map.get(key)==1) answer=key; //남은 사람이 미완주자
         }
+        
         return answer;
-
     }
 }
+
+
