bcb44e589990a8d74b5c73a3b7bc05c186631994
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-01 21:36:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.80 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,26 +1,14 @@
 class Solution{
     public String solution(String s){
         String answer="";
-        int mid=s.length()/2;
+        int n=s.length();
+        int mid=n/2;
         
-        if(s.length()%2==1){
-            answer=s.substring(mid,mid+1);
+        if(n%2==0){ //짝수
+            answer+=s.substring(mid-1,mid+1);
+        }else{ //홀수
+            answer+=s.substring(mid,mid+1);
         }
-        else{  //짝수
-            answer=s.substring(mid-1,mid+1);
-        }
-        
-        return answer;   
+        return answer;
     }
 }
-
-/*
-홀수
-0 1 2 3 4 
-mid=길이/2 =>2 , 2번인덱스 ok
-
-짝수
-0 1 2 3 4 5
-mid=3번 + -1번인덱스
-mid-1 ~ mid 
-*/
