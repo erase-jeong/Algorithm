eccb6c51acd0d83ef61c920be0bc716bee7454aa
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-06 12:50:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.93 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,46 @@
 import java.util.*;
 
 class Solution{
+    int[] dx={-1,1,0,0}; //static으로 안해도 괜찮? why?
+    int[] dy={0,0,-1,1};
     
     public int solution(int[][] maps){
         int n=maps.length;
         int m=maps[0].length;
         
-        int[] dx={-1,1,0,0};
-        int[] dy={0,0,-1,1};
-        
-        Queue<int []> queue=new LinkedList<>();
         boolean[][] visited=new boolean[n][m];
-        
+        return bfs(maps, visited, n, m);
+    }
+    
+    public int bfs(int[][] maps, boolean[][] visited, int n, int m){
+        Queue<int[]> queue=new LinkedList<>();
         
         queue.add(new int[]{0,0,1});
+        visited[0][0]=true;
         
         while(!queue.isEmpty()){
-            int[] p=queue.poll();
-            int x=p[0];
-            int y=p[1];
-            int cnt=p[2];
-            //visited[x][y]=true;  //(1)
+            int[] current=queue.poll();
+            int x=current[0];
+            int y=current[1];
+            int cnt=current[2];
             
             if(x==n-1 && y==m-1){
                 return cnt;
             }
             
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
+            for(int d=0;d<4;d++){
+                int nx=x+dx[d];
+                int ny=y+dy[d];
                 
-                if(nx>=0 && ny>=0 && nx<n && ny<m && maps[nx][ny]==1 && !visited[nx][ny]){
-                    visited[nx][ny]=true;  //같은 값 다시 큐에 넣지 않기 위해서 
+                if(0<=nx && nx<=n-1 && 0<=ny && ny<=m-1 
+                   && maps[nx][ny]==1 && !visited[nx][ny]){
+                    visited[nx][ny]=true;
                     queue.add(new int[]{nx,ny,cnt+1});
-                }
-                
+                    //count++;
+                } 
             }
-            
         }
-        return -1;
+        return -1;     
     }
-    
 }
+
