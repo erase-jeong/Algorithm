5285062beafc2167db522f4aa088de566aeff593
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-06 23:02:05 +0900

    [level 3] Title: 여행경로, Time: 75.20 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution{
     static List<String> list=new ArrayList<>();
-    static boolean[] useTicket;
+    static boolean useTickets[];
     
     public String[] solution(String[][] tickets){
-        useTicket=new boolean[tickets.length];
+        useTickets=new boolean[tickets.length];
         dfs(0,"ICN","ICN",tickets);
         Collections.sort(list);
+        
         return list.get(0).split(" ");
     }
     
-    public static void dfs(int depth, String now, String path,String[][] tickets){
+    public static void dfs(int depth, String now, String path, String[][] tickets){
         
         if(depth==tickets.length){
             list.add(path);
             return;
         }
         
-        for(int i=0;i<useTicket.length;i++){
-            if(!useTicket[i] && now.equals(tickets[i][0])){
-            useTicket[i]=true;
-            dfs(depth+1,tickets[i][1],path+" "+tickets[i][1],tickets);
-            useTicket[i]=false;
+        for(int i=0;i<tickets.length;i++){
+            if(!useTickets[i] && now.equals(tickets[i][0])){
+                useTickets[i]=true;
+                dfs(depth+1, tickets[i][1], path+" "+tickets[i][1],tickets);
+                useTickets[i]=false;
             }
         }
         
     }
+    
 }
