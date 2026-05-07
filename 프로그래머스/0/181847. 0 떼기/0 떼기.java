0af70532b105a4f8a6fbbac205877649a2aee1ee
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 05:13:45 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        
-        while(true){
-            if(n_str.charAt(0)=='0'){
-                n_str=n_str.substring(1);
-            }else{
-                break;
-            }
-        }
-        
-        return n_str;
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
