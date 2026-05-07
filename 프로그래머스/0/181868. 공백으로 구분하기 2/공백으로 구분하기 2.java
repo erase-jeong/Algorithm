6d515abbbbcfc63080c178900d489d9c4335de3c
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 05:08:53 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.71 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,7 +1,9 @@
 class Solution {
     public String[] solution(String my_string) {
-        my_string=my_string.trim();
-        String[] answer=my_string.split("\\s+");
+        //my_string=my_string.trim();
+        //String[] answer=my_string.split("\\s+");
+        //return answer;
+        String[] answer=my_string.trim().split("\\s+");
         return answer;
     }
 }
