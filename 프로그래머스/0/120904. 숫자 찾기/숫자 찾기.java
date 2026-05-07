4f7624ac19881dec178a2629399cff0190ee462f
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 05:41:15 +0900

    [level 0] Title: 숫자 찾기, Time: 0.10 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int num, int k) {
-        int answer=-1;
-        
+        int answer=-1; //초기값을 -1로 설정
         String str=Integer.toString(num);
+        //String str=String.valueOf(num);
         String[] arr=str.split("");
-      
+        
         for(int i=0;i<arr.length;i++){
-            if(arr[i].equals(Integer.toString(k))){
-                answer=i+1;
+            if(arr[i].equals(String.valueOf(k))){
+                answer=i+1; //정답을 찾았을 때 answer값을 업데이트하고 루프를 빠져나감
                 break;
             }
         }
