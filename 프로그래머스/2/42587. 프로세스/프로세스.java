31cf52e0cb2a4092bab9a43a9591953b1510635c
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 15:08:30 +0900

    [level 2] Title: 프로세스, Time: 1.20 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 우선순위 큐 선언(내림 차순)
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        int answer = 0;
-
-        // 우선순위 큐에 우선순위 요소 추가
-        for (int i : priorities) {
-            queue.offer(i);
+        //우선순위 큐 선언(내림 차순)
+        PriorityQueue<Integer> q=new PriorityQueue<>(Collections.reverseOrder());
+        int answer=0;
+        
+        //우선순위 큐에 우선순위 요소 추가
+        for(int i : priorities){
+            q.offer(i);
         }
-
-        // 큐가 빌 때까지 반복
-        while (!queue.isEmpty()) {
-            // 기존 우선순위 배열 순회
-            for (int i = 0; i < priorities.length; i++) {
-                // 현재 작업의 위치 찾기
-                if (queue.peek() == priorities[i]) {
-                    queue.poll();
+        System.out.println(q);
+        
+        //큐가 빌때까지 반복
+        while(!q.isEmpty()){
+            //System.out.println(q.poll());
+            //기존 우선순위 배열 순회
+            for(int i=0;i<priorities.length;i++){
+                //현재 작업의 위치 찾기
+                if(q.peek()==priorities[i]){
+                    q.poll();
                     answer++;
-
-                    // 현재 작업이 location과 같으면 answer 반환
-                    if (location == i) {
-                        return answer;
-                    }
+                    
+                    //현재 작업이 location과 같으면 answer 반환
+                    if(location==i) return answer;
                 }
             }
         }
-
+        
         return answer;
     }
 }
