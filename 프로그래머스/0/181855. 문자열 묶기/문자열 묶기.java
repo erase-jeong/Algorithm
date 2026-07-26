edc50f87e78b20b13b6361a30d6eb4c829436593
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-27 08:38:58 +0900

    [level 0] Title: 문자열 묶기, Time: 5.99 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {
-        int answer=0;
-        
-        int[] length=new int[31];
+        //배열 만들기
+        int[] size=new int[31];
         
         for(int i=0;i<strArr.length;i++){
-            int value=strArr[i].length();
-            length[value]++;
-        }   
-        
-        int maxV=0;
-        for(int i=1;i<=30;i++){
-            if(maxV<length[i]) maxV=length[i];
+            int n=strArr[i].length();
+            size[n]++;
         }
-        return maxV;
+        
+        Arrays.sort(size);
+        
+        return size[30];
     }
 }
