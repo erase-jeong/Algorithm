738f8471d04ddb77f3ecf721799bf135cf26bc1c
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-30 22:44:45 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,16 +1,36 @@
 class Solution {
     public String[] solution(String[] names) {
-        int n;
-        if(names.length%5==0) n=names.length/5;
-        else n=names.length/5+1;
+        int n=names.length/5;
+        if(names.length%5!=0) n++;
         
         String[] answer=new String[n];
         
-        int j=0;
-        for(int i=0;i<n;i++){ //0, 5, 10 처럼 5의 배수 값을 넣는다
-            answer[i]=names[j];
-            j+=5;
+        for(int i=0;i<n;i++){
+            answer[i]=names[i*5];
         }
         return answer;
     }
 }
+
+/*
+배열 전체의 길이 
+그 중에서 5씩 건너뛰면서 배열인덱스 조사? 갱신
+배열 사이즈 끝나기 전까지 반복하기
+
+1. 전체 배열 길이를 순회한다.
+2. 그 중에서 5씩 건너뛰면서 한다. 
+남은 배열길이가 0이 아닐때까지 반복하기
+
+7%5=1+...2 => 2
+5%5=1+...0 => 1
+
+1. result의 길이 = 몫(names.length/5) + 
+    if(나머지가 존재){
+        //+1 더한다
+    }else{  //나머지가 없다면 
+        //안더한다
+    }
+    
+2. 5*idx
+0, 5, 10 번째를 배열에 넣는다. 
+*/
--- 프로그래머스/0/181886. 5명씩/README.md
+++ 프로그래머스/0/181886. 5명씩/README.md
@@ -1,10 +1,10 @@
 # [level 0] 5명씩 - 181886 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.02 ms
+메모리: 75 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 02일 20:45:50
+2026년 07월 30일 22:44:42
 
 ### 문제 설명
 
